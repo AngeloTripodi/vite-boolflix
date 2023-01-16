e4d44cdcc09f09ae4356db30d5dd5b457cf4b85b
AngeloTripodi <tripodi.angelo@gmail.com>
2023-01-16 22:31:16 +0100

    Add cover img

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -5,6 +5,7 @@ export default {
     data() {
         return {
             store,
+            imgSize: 'w185'
         }
     },
     methods: {
@@ -33,6 +34,10 @@ export default {
                 return 'No flag for ' + movieLanguage + ' :(';
             }
 
+        },
+
+        createCover() {
+
         }
     },
 
@@ -43,12 +48,14 @@ export default {
     <main>
         <h1>Main</h1>
         <div v-for="movie in store.moviesList ">
+            <img :src="`https://image.tmdb.org/t/p/${imgSize}${movie.poster_path}`" alt="">
             {{ movie.title }} {{ movie.name }}/ {{ movie.original_title }} {{ movie.original_name }}/ -- <img
                 :src="flags(movie.original_language)" :alt="flags(movie.original_language)">
             -- {{ movie.vote_average }}
         </div>
 
-        <div v-for="show in store.showsList "> PORCO
+        <div v-for="show in store.showsList ">
+            <img :src="`https://image.tmdb.org/t/p/${imgSize}${show.poster_path}`" alt="">
             {{ show.name }} / {{ show.original_name }} / <img :src="flags(show.original_language)"
                 :alt="flags(show.original_language)">
             -- {{ show.vote_average }}
